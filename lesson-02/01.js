bd57ffae763302c6df2fde8b8d3c980eab3fc019
Ivan Rusy <ivan.rusy@gmail.com>
2026-01-18 12:11:08 +0300

    second dz for js

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -13,6 +13,6 @@ const isVerifiedUser = true
 const hasSpecialPermission = true
 const hasTemporaryPass = false
 
-let isAccess
+let isAccess;
+if ((isAdmin or isVerifiedUser)and (hasSpecialPermission or hasTemporaryPass)){isAccess = true} else { isAccess = false}
 
-// your code
--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,13 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
-let grade
+const score = 70 ;// тестовое значение, можно изменять
+let grade;
+if (score >=0 and score <=49) {grade ='F'}
+else if (score >=50 and score <=69) {grade ='D'}
+else if (score >=70 and score <=79) {grade ='C'}
+else if (score >=80 and score <=89) {grade ='B'}
+else if (score >=90 and score <=100) {grade ='A'}	
+else grade = 'Некорректный бал, введите значение от 0 до 100'
+	
 // your code
